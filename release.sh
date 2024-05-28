d870391f49657cc68f94323d935f0da4dd4702ad
yinlei <yinlei@axzo.cn>
2024-05-28 22:30:33 +0800

    test

--- release.sh
+++ release.sh
@@ -7,6 +7,7 @@ hexo clean && hexo generate
 git add .
 git commit -m "$content"
 git push
+rm -rf /home/ylighgh/workspace/github/ylighgh.github.io/hexo/*
 cp -r /home/ylighgh/workspace/github/hexo/public/* /home/ylighgh/workspace/github/ylighgh.github.io/hexo/
 echo "=====================（2/2）======================================"
 cd /home/ylighgh/workspace/github/ylighgh.github.io/
