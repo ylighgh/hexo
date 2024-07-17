58d741c4d654ee88c91ce66d7b97918416a1fb2d
ylighgh <yinlei@axzo.cn>
2024-07-17 16:49:37 +0800

    update release.sh

--- release.sh
+++ release.sh
@@ -3,18 +3,18 @@ read -p """Please enter the updated content:" content
 
 echo "=====================（1/2）====================================="
 cd /home/ylighgh/workspace/github/hexo/
-git pull
 hexo clean && hexo generate
 git add .
 git commit -m "$content"
+git pull
 git push
 rm -rf /home/ylighgh/workspace/github/ylighgh.github.io/hexo/*
 cp -r /home/ylighgh/workspace/github/hexo/public/* /home/ylighgh/workspace/github/ylighgh.github.io/hexo/
 echo "=====================（2/2）======================================"
 cd /home/ylighgh/workspace/github/ylighgh.github.io/
-git pull
 git add .
 git commit -m"$content"
+git pull
 git push
 echo "============================Finshed==============================="
 exit 0
